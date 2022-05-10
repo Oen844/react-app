9b3ef7bb959fdba77263c919fac8b5263c77927f
Pau Egea <papau84@gmail.com>
2022-05-10 21:54:20 +0200

    components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import React from "react";
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { CreateTodoButton } from "./CreateTodoButton";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
 
-//image sega
-const imageSrcSega = "https://i.imgur.com/EOwGBFH.png"
+const todos = [
+  {
+    text: "Cortar cebolla",
+    completed: false,
+  },
+  {
+    text: "Aprender React",
+    completed: false,
+  },
+  {
+    text: "Entrevista de trabajo",
+    completed: false,
+  },
+];
 
 function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Edita <code>src/App.js</code> and save to reload.
-        </p>
-        <img src={imageSrcSega} alt="Sega" width="50%" />
-        <a
-          className="App-link"
-          href="https://github.com/Oen844"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Sega<br></br> 
-          ponte a estudiar {props.saludo}
-          
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter />
+      
+      <TodoSearch />
+     
+      <TodoList>
+        {todos.map((todo) => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+      <CreateTodoButton /> 
+      {/* <button>+</button> */}
+    </React.Fragment>
   );
 }
 
