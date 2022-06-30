eb70413fbad6221d423ffe9fa3013b221cf04259
Pau Egea <papau84@gmail.com>
2022-06-30 18:27:37 +0200

    Updates

---
+++ precache-manifest.4f9d43ff59327144c904be05a05d781c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac4a91ef0b88913b078a",
+    "url": "/react-app/static/css/main.ca85b83c.chunk.css"
+  },
+  {
+    "revision": "ac4a91ef0b88913b078a",
+    "url": "/react-app/static/js/main.ac4a91ef.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/react-app/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "8f9f978bc0eefbf43087",
+    "url": "/react-app/static/js/2.8f9f978b.chunk.js"
+  },
+  {
+    "revision": "6eee2c7f893e99f7b272",
+    "url": "/react-app/static/js/runtime~main.6eee2c7f.js"
+  },
+  {
+    "revision": "f45071736ed63fdb48530200e02de397",
+    "url": "/react-app/index.html"
+  }
+];
